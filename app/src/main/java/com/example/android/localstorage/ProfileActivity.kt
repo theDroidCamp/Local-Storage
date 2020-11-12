32b33a38369f64b361a13bc1eea6e740d028956a
Bakare Khadijat <72657345+Voiced-debug@users.noreply.github.com>
2020-11-12 13:28:35 +0100

    Allowed for more than one user

--- app/src/main/java/com/example/android/localstorage/ProfileActivity.kt
+++ app/src/main/java/com/example/android/localstorage/ProfileActivity.kt
@@ -2,8 +2,8 @@ package com.example.android.localstorage
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -12,7 +12,7 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_profile)
 
 
 
